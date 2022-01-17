e95509858517b09df52bdc092aeed89e9e72ddfa
insujie <ultra1945@gmail.com>
2022-01-17 10:50:14 +0900

    modify sh files

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,7 +6,7 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR/profile.sh}
 
 REPOSITORY=/home/ec2-user/app/step2
-PROJECT_NAME=yoon-springboot2-webservice
+PROJECT_NAME=survey
 
 echo "> Build 파일 복사"
 echo "> $REPOSITORY/zip/*.jar $REPOSITORY"
@@ -30,6 +30,6 @@ IDLE_PROFILE=$(find_idle_profile)
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.yml,classpath:/application-$IDLE_PROFILE.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml \
-    -Dspring.profiles.active=$IDLE_PROFILE \
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+      -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-real-db.properties \
+      -Dspring.profiles.active=$IDLE_PROFILE \
+      $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
